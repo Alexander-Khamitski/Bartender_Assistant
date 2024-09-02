c85b61f94825715c4c3985831eef0b6ff82f14d8
Alexander Khamitski <alexander.khamitski@winfinity.live>
2024-09-03 01:11:56 +0300

    initial commit

---
+++ src/main/java/com/teachmeskills/bartender_assistant/BartenderAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.teachmeskills.bartender_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BartenderAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BartenderAssistantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teachmeskills/bartender_assistant/BartenderAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teachmeskills.bartender_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BartenderAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
